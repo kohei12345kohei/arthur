38f12b7b662232157864f610805fee061f3b9553
kohei12345kohei <kohei12345kohei@gmail.com>
2018-03-17 22:19:06 +0900

    修正

--- arthur-common/src/main/java/jp/co/arthur/common/dao/impl/LoginUserDaoImpl.java
+++ arthur-common/src/main/java/jp/co/arthur/common/dao/impl/LoginUserDaoImpl.java
@@ -1,11 +1,21 @@
 package jp.co.arthur.common.dao.impl;
 
-import java.util.Date;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.stereotype.Repository;
 
 import jp.co.arthur.common.dao.LoginUserDao;
 import jp.co.arthur.common.entity.LoginUser;
+import jp.co.arthur.common.other.OsDefine;
+import jp.co.arthur.common.util.DateUtil;
 
 /**
  * ログインユーザDao実装クラス<br>
@@ -14,6 +24,9 @@ import jp.co.arthur.common.entity.LoginUser;
 @Repository
 public class LoginUserDaoImpl implements LoginUserDao {
 
+	/** 保存先ファイルパス */
+	public static final String RESOURCES = OsDefine.isWin() ? "C:\\work\\arthur_db.xlsx" : "/Applications/arthur_db.xlsx";
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -21,10 +34,35 @@ public class LoginUserDaoImpl implements LoginUserDao {
 	public LoginUser findLoginUserByLoginId(String loginId) {
 
 		LoginUser loginUser = new LoginUser();
-//		loginUser.setLoginId(loginId);
-//		loginUser.setPassword("password");
-//		loginUser.setRegDate(new Date());
-//		loginUser.setUpdateDate(new Date());
+
+		try (Workbook workbook = WorkbookFactory.create(new File(RESOURCES))) {
+
+			Sheet sheet = workbook.getSheet(TABLE_NAME);
+			Iterator<Row> iteRow = sheet.rowIterator();
+			while (iteRow.hasNext()) {
+
+				// 1行取得
+				Row row = iteRow.next();
+
+				// ヘッダーの場合、次のレコードに進む
+				if (row.getRowNum() == 0) continue;
+
+				if (loginId.equals(row.getCell(0).getStringCellValue())) {
+
+					loginUser.setLoginId(row.getCell(0).getStringCellValue());
+					loginUser.setPassword(row.getCell(1).getStringCellValue());
+					loginUser.setRegDate(DateUtil.formatDate(row.getCell(2).getStringCellValue()));
+					loginUser.setUpdateDate(DateUtil.formatDate(row.getCell(3).getStringCellValue()));
+				}
+			}
+
+		} catch (EncryptedDocumentException e) {
+			e.printStackTrace();
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		return loginUser;
 	}
