e5556f69f6d8410a6559325eee337913f318ce7e
kohei12345kohei <kohei12345kohei@gmail.com>
2018-03-20 14:57:55 +0900

    no message

---
+++ arthur-api/src/main/java/jp/co/arthur/api/service/PurchaseRegistService.java
@@ -0,0 +1,10 @@
+package jp.co.arthur.api.service;
+
+/**
+ * 購入商品登録サービス<br>
+ *
+ */
+public interface PurchaseRegistService {
+
+	void execute();
+}
---
+++ arthur-common/src/main/java/jp/co/arthur/common/entity/PurchaseInfo.java
@@ -0,0 +1,106 @@
+package jp.co.arthur.common.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class PurchaseInfo {
+
+	/** アカウント */
+	private String account;
+	/** 商品ID */
+	private String itemId;
+	/** 商品名 */
+	private String itemName;
+	/** 商品金額(単品) */
+	private BigDecimal itemUnitMoney;
+	/** 商品数 */
+	private BigDecimal itemAmount;
+	/** 登録日時 */
+	private Date regDate;
+
+	/**
+	 * accountを返す<br>
+	 * @return account
+	 */
+	public String getAccount() {
+		return account;
+	}
+	/**
+	 * accountを設定する<br>
+	 * @param account
+	 */
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	/**
+	 * itemIdを返す<br>
+	 * @return itemId
+	 */
+	public String getItemId() {
+		return itemId;
+	}
+	/**
+	 * itemIdを設定する<br>
+	 * @param itemId
+	 */
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	/**
+	 * itemNameを返す<br>
+	 * @return itemName
+	 */
+	public String getItemName() {
+		return itemName;
+	}
+	/**
+	 * itemNameを設定する<br>
+	 * @param itemName
+	 */
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	/**
+	 * itemUnitMoneyを返す<br>
+	 * @return itemUnitMoney
+	 */
+	public BigDecimal getItemUnitMoney() {
+		return itemUnitMoney;
+	}
+	/**
+	 * itemUnitMoneyを設定する<br>
+	 * @param itemUnitMoney
+	 */
+	public void setItemUnitMoney(BigDecimal itemUnitMoney) {
+		this.itemUnitMoney = itemUnitMoney;
+	}
+	/**
+	 * itemAmountを返す<br>
+	 * @return itemAmount
+	 */
+	public BigDecimal getItemAmount() {
+		return itemAmount;
+	}
+	/**
+	 * itemAmountを設定する<br>
+	 * @param itemAmount
+	 */
+	public void setItemAmount(BigDecimal itemAmount) {
+		this.itemAmount = itemAmount;
+	}
+	/**
+	 * regDateを返す<br>
+	 * @return regDate
+	 */
+	public Date getRegDate() {
+		return regDate;
+	}
+	/**
+	 * regDateを設定する<br>
+	 * @param regDate
+	 */
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+}
