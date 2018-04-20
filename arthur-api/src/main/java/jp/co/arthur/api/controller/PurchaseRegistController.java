f28b2f5129a58a8df6f5e270104a4a52c873a920
kohei12345kohei <kohei12345kohei@gmail.com>
2018-04-21 00:56:16 +0900

    修正

--- arthur-api/src/main/java/jp/co/arthur/api/controller/PurchaseRegistController.java
+++ arthur-api/src/main/java/jp/co/arthur/api/controller/PurchaseRegistController.java
@@ -2,10 +2,12 @@ package jp.co.arthur.api.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import jp.co.arthur.api.request.PurchaseRegistRequest;
 import jp.co.arthur.api.response.PurchaseRegistResponse;
+import jp.co.arthur.api.service.PurchaseRegistService;
 import jp.co.arthur.common.api.BaseApiRestController;
 import jp.co.arthur.common.exception.BaseArthurException;
 
@@ -16,12 +18,23 @@ import jp.co.arthur.common.exception.BaseArthurException;
 @RestController
 public class PurchaseRegistController implements BaseApiRestController<PurchaseRegistRequest, PurchaseRegistResponse> {
 
+
+	@Autowired
+	private PurchaseRegistService purchaseRegistService;
+
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public PurchaseRegistResponse execute(PurchaseRegistRequest request) throws BaseArthurException {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+
+		PurchaseRegistResponse response = purchaseRegistService.execute(request);
+		return response;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public PurchaseRegistRequest toRequest(HttpServletRequest request) throws BaseArthurException {
 
