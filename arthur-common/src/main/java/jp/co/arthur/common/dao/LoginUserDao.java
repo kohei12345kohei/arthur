fdd176c28bf9c2265c74483e93489e770f587284
kohei12345kohei <kohei12345kohei@gmail.com>
2018-06-02 11:55:23 +0900

    修正

--- arthur-common/src/main/java/jp/co/arthur/common/dao/LoginUserDao.java
+++ arthur-common/src/main/java/jp/co/arthur/common/dao/LoginUserDao.java
@@ -13,21 +13,27 @@ public interface LoginUserDao {
 
 	/**
 	 * ログインIDからログインユーザを返す<br>
+	 *
 	 * @param loginId
+	 *            ログインID
 	 * @return
 	 */
 	LoginUser findByLoginId(String loginId);
 
 	/**
 	 * アカウントからログインユーザを返す<br>
+	 *
 	 * @param account
+	 *            アカウント
 	 * @return
 	 */
 	LoginUser findByAccount(String account);
 
 	/**
 	 * 作成
+	 *
 	 * @param entity
+	 *            LoginUser
 	 */
 	void create(LoginUser entity);
 
